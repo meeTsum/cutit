c779279e43b12fd52b75107601b9faa07e31eaf1
unknown <rebeccamanegbo@gmail.com>
2026-04-27 17:02:02 +0000

    dev : add feature

--- src/app/shared/components/ui/how-it-works/how-it-works.ts
+++ src/app/shared/components/ui/how-it-works/how-it-works.ts
@@ -9,5 +9,31 @@ import { LucideModule } from '../../../lucide.module';
   styleUrl: './how-it-works.css',
 })
 export class HowItWorks {
-
+  steps = [
+    {
+      title: 'Sélectionner une vidéo',
+      description: 'Importer depuis votre galerie',
+      icon: 'circle-plus'
+    },
+    {
+      title: 'Ajustez la durée',
+      description: 'Définissez vos préférence de temps en un clic',
+      icon: 'circle-plus'
+    },
+    {
+      title: 'Découpage automatique',
+      description: "Cutit s'occupe de découper avec précision vos vidéos",
+      icon: 'scissors'
+    },
+    {
+      title: 'Sauvegarder',
+      description: 'Choisissez vos segments à enregistrer ou exporter',
+      icon: 'circle-plus'
+    },
+    {
+      title: 'Partager',
+      description: 'Directement sur vos réseaux Whatsapp, tiktok, Youtube, Insta, FB etc...',
+      icon: 'circle-plus'
+    }
+  ];
 }
